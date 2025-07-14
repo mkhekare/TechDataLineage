7e98348e6ab201d0f4c586536979044ebb0c0fe8
Mayur Khekare <mkhekare@gmail.com>
2025-07-15 01:36:36 +0530

    heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './styles/App.css';
 
