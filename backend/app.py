6f9577d2573d2811ad84c19ee5511211ead28530
Mayur Khekare <mkhekare@gmail.com>
2025-07-15 01:52:57 +0530

    kharcha

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from controllers.lineage_controller import LineageController
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
     # Initialize the LineageController
     lineage_controller = LineageController()
@@ -10,6 +10,10 @@ def create_app():
     # Set up routes
     app.add_url_rule('/upload', view_func=lineage_controller.upload_file, methods=['POST'])
     app.add_url_rule('/lineage-report', view_func=lineage_controller.get_lineage_report, methods=['GET'])
+
+    @app.route('/')
+    def serve():
+        return send_from_directory(app.static_folder, 'index.html')
 
     return app
 
