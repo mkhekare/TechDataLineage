2d5fb1ffee8d436735c8a89bc5cf6a13a889b2cd
Mayur Khekare <mkhekare@gmail.com>
2025-07-15 01:21:48 +0530

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </Router>
   );
 }
