112348d6abbb15bc3d4bfa1f65cbbaad2be2b085
Mayur Khekare <mkhekare@gmail.com>
2025-07-15 01:09:54 +0530

    gemini changes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import FileUpload from '../components/FileUpload';
-import RepositoryLinkInput from '../components/RepositoryLinkInput';
 import LineageGraph from '../components/LineageGraph';
 
 const HomePage = () => {
     return (
         <div>
             <h1>AI-Powered Data Lineage Generator</h1>
-            <FileUpload />
-            <RepositoryLinkInput />
             <LineageGraph />
         </div>
     );
